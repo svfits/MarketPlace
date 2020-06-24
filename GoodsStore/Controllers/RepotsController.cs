2b92e97bdba918261f7e903e4f75c26936048e1a
Дмитрий Афанасьев <a1d1@inbox.ru>
2020-06-24 17:27:33 +0800

    PDF Create

---
+++ GoodsStore/Controllers/RepotsController.cs
@@ -0,0 +1,146 @@
+﻿using System.IO;
+using System.Linq;
+using GoodsStore.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System.Collections.Generic;
+using PdfSharp.Fonts;
+
+namespace GoodsStore.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class RepotsController : Controller
+    {
+        private readonly IDataContextApp _context;
+
+        public RepotsController(IDataContextApp context)
+        {
+            _context = context;
+        }
+
+        public IActionResult DownloadExcel()
+        {
+            byte[] fileContents;
+            var productInBasket = GetProductBasket();
+
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+
+                worksheet.Cells[1, 1].Value = "Наименование товара";
+                worksheet.Cells[1, 2].Value = "Артикул";
+                worksheet.Cells[1, 3].Value = "Категория";
+                worksheet.Cells[1, 4].Value = "Стоимость";
+                worksheet.Cells[1, 5].Value = "Email заказчика";
+
+                //Раскрасим шапку
+                for (int i = 1; i <= 5; i++)
+                {
+                    worksheet.Cells[1, i].Style.Font.Size = 12;
+                    worksheet.Cells[1, i].Style.Font.Bold = true;
+                    worksheet.Cells[1, i].Style.Border.Top.Style = ExcelBorderStyle.Hair;
+                }
+
+                for (int i = 2; i < (productInBasket.Count + 2); i++)
+                {
+                    worksheet.Cells[i, 1].Value = productInBasket[i - 2].Product.NameProduct;
+                    worksheet.Cells[i, 2].Value = productInBasket[i - 2].Product.Id;
+                    worksheet.Cells[i, 3].Value = productInBasket[i - 2].Product.Category.NameCategory;
+                    worksheet.Cells[i, 4].Value = productInBasket[i - 2].Product.Cost;
+                    worksheet.Cells[i, 5].Value = productInBasket[i - 2].User.Email;
+                }
+
+                int count = (productInBasket.Count + 2);
+                worksheet.Cells[count, 5].Value = "Итого товаров:" + productInBasket.Count;
+                worksheet.Cells[count, 5].Style.Font.Size = 16;
+                worksheet.Cells[count, 5].Style.Font.Bold = true;
+                worksheet.Cells[count, 5].Style.Border.Top.Style = ExcelBorderStyle.Hair;
+
+                int countSum = (productInBasket.Count + 3);
+                worksheet.Cells[countSum, 5].Value = "На сумму:" + productInBasket.Sum(a => a.Product.Cost);
+                worksheet.Cells[countSum, 5].Style.Font.Size = 16;
+                worksheet.Cells[countSum, 5].Style.Font.Bold = true;
+                worksheet.Cells[countSum, 5].Style.Border.Top.Style = ExcelBorderStyle.Hair;
+
+                fileContents = package.GetAsByteArray();
+            }
+
+            if (fileContents == null || fileContents.Length == 0)
+            {
+                return NotFound();
+            }
+
+            return File(
+                fileContents: fileContents,
+                contentType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                fileDownloadName: "Заказанные товары.xlsx"
+            );
+        }
+
+        private List<Basket> GetProductBasket()
+        {
+            return _context.Baskets
+                .Include(a => a.User)
+                .Include(q => q.Product)
+                .Include(o => o.Product.Category)
+                .ToList();
+        }
+
+        public IActionResult DownloadPdf()
+        {
+            byte[] fileContents;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            // Create a new PDF document
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "Сделано по заказу";
+
+            // Create an empty page
+            PdfPage page = document.AddPage();
+
+            // Get an XGraphics object for drawing
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            // Create a font
+            XFont font = new XFont("Verdana", 20, XFontStyle.BoldItalic);
+
+            // Draw the text
+            //gfx.DrawString("Привет МИР!!! ККККККККККККККККкккккккккккккк", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+            //gfx.DrawString("Привет МИР!!! ККККККККККККККККкккккккккккккк", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+            //gfx.DrawString("Привет МИР!!! ККККККККККККККККкккккккккккккк", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+            //gfx.DrawString("Привет МИР!!! ККККККККККККККККкккккккккккккк", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+            //gfx.DrawString("Привет МИР!!! ККККККККККККККККкккккккккккккк", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+            //gfx.DrawString("Привет МИР!!! ККККККККККККККККкккккккккккккк", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+            //gfx.DrawString("Привет МИР!!! ККККККККККККККККкккккккккккккк", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+            //gfx.DrawString("Привет МИР!!! ККККККККККККККККкккккккккккккк", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+            //gfx.DrawString("Привет МИР!!! ККККККККККККККККкккккккккккккк", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+            //gfx.DrawString("Привет МИР!!! ККККККККККККККККкккккккккккккк", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+            gfx.DrawString("Привет МИР!!! ККККККККККККККККкккккккккккккк", font, XBrushes.Black, 22, 45);
+                        
+            //gfx.DrawString("Привет МИР!!! ККККККККККККККККкккккккккккккк", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.BaseLineRight);
+            //gfx.DrawString("Привет МИР!!! ККККККККККККККККкккккккккккккк", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.BaseLineLeft);
+
+
+            MemoryStream ms = new MemoryStream();
+            document.Save(ms);
+            fileContents = ms.ToArray();
+            ms.Dispose();
+
+            if (fileContents == null || fileContents.Length == 0)
+            {
+                return NotFound();
+            }
+
+            return File(
+                fileContents: fileContents,
+                contentType: "application/pdf",
+                fileDownloadName: "Заказанные товары.pdf"
+            );
+        }
+    }
+}
---
+++ GoodsStoreTests/Controllers/ReportsControllerTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GoodsStore.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using GoodsStore.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace GoodsStore.Controllers.Tests
+{
+    [TestClass()]
+    public class ReportsControllerTests
+    {
+        Mock<IDataContextApp> mockContext = new Mock<IDataContextApp>();
+
+        RepotsController service;
+
+        [TestInitialize]
+        [DeploymentItem("TestingFile\\DataContextTEST.json")]
+        private void MockContext()
+        {
+            using StreamReader r = new StreamReader("DataContextTEST.json");
+            string json = r.ReadToEnd();
+
+            var data = JsonConvert.DeserializeObject<List<Basket>>(json).AsQueryable();
+
+            var mockSet = new Mock<DbSet<Basket>>();
+
+            mockSet.As<IQueryable<Basket>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<Basket>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Basket>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Basket>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            mockContext.Setup(c => c.Baskets).Returns(mockSet.Object);
+            service = new RepotsController(mockContext.Object);
+        }
+
+
+        [TestMethod()]
+        [DeploymentItem("TestingFile\\Заказанные товары.xlsx")]
+        public void ExcelControllerTest()
+        {
+            var excelByte = (Microsoft.AspNetCore.Mvc.FileContentResult)service.DownloadExcel();
+
+            ///тута можно отправить файл дальше на проверку что форматирование правильное
+            File.WriteAllBytes("TestingFile\\FileTarget.xlsx", excelByte.FileContents);
+
+            var file = File.ReadAllBytes("Заказанные товары.xlsx");
+
+            Assert.AreEqual(excelByte.FileContents.Count(), file.ToList().Count, "Файлы не совпадают что то произошло");
+        }
+
+
+
+        //public List<Basket> LoadTestData(string fileName)
+        //{
+        //    using StreamReader r = new StreamReader(fileName);
+        //    string json = r.ReadToEnd();
+        //    return JsonConvert.DeserializeObject<List<Basket>>(json);
+        //}
+
+        //public byte[] LoadTestFileSample(string fileName)
+        //{
+        //    var file = File.ReadAllBytes(fileName);
+        //    return file;
+        //}
+
+        [TestMethod()]
+        public void DownloadPdfTest()
+        {
+            var service = new RepotsController(mockContext.Object);
+            var pdfFile = (Microsoft.AspNetCore.Mvc.FileContentResult)service.DownloadPdf();
+
+            ///тута можно отправить файл дальше на проверку что форматирование правильное
+            File.WriteAllBytes(@"D:\Projects\MarketPlace\GoodsStoreTests\TestingFile\ggggg22.pdf", pdfFile.FileContents);
+
+            Assert.IsTrue(true);
+        }
+    }
+}
